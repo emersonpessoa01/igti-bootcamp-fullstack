b453814791867a2de966049417479b4e8e2eabc5
emersonpessoa01 <eps011108@gmail.com>
2020-08-24 10:01:30 -0300

    upload

---
+++ modulo03/meu-exercicio/javascript-prefomatico/js/script-exerc.js
@@ -0,0 +1,33 @@
+let clickArray = [];
+
+window.addEventListener('load', () => {
+    document.querySelector('#clickButton').addEventListener('click', () => {
+        // const now = new Date();
+        // clickArray.push(now.toISOString());
+        const item = getNewTimestamp();
+        clickArray.push(item);
+
+        // console.log(clickArray);
+        render(item);
+    });
+});
+
+const render = (item) => {
+    const ul = document.querySelector('#data');
+
+    const li = document.createElement('li');
+    li.innerHTML = item;
+
+    ul.appendChild(li);
+
+
+    // ul.innerHTML = '';
+    // let lis = '';
+
+    // clickArray.map((item) => {
+    //     lis += `<li>${item}</li>`
+    // });
+
+    // ul.innerHTML = lis;
+    // document.title = clickArray.length;
+}
