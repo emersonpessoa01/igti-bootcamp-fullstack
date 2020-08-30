0c4f18a7c3232e3af9ad8eedb957dbf81a1a333a
emersonpessoa01 <eps011108@gmail.com>
2020-08-30 14:57:05 -0300

    update only gifs 02

--- modulo01/README.md
+++ modulo01/README.md
@@ -1,17 +1,17 @@
-# Visão geral das aplicações do módulo:
+# Over view das aplicações do módulo:
 <hr>
-     <img src="./image/images/CRUD.gif" alt="crud">
+     <img src="./image/gifs/CRUD.gif" alt="crud">
 <hr>
      <p align="center">
-     <img src="image/images/color-selector.gif" alt="color-selector">
+     <img src="image/gifs/color-selector.gif" alt="color-selector">
      </p>
 <hr>
-     <img src="./image/images/rangerColor.gif" alt="rangerColor">
+     <img src="./image/gifs/rangerColor.gif" alt="rangerColor">
 <hr>
-     <img src="./image/images/jsperfomatico.gif" alt="jsperformatico">
+     <img src="./image/gifs/jsperfomatico.gif" alt="jsperformatico">
 <hr>
      <p align="center">
-     <img src="./image/images/radio-podcast.gif" alt="radio-podcast">
+     <img src="./image/gifs/radio-podcast_2.gif" alt="radio-podcast">
      </p>
 <hr>
 
--- modulo01/radio-podcast/js/podcast.js
+++ modulo01/radio-podcast/js/podcast.js
@@ -8,77 +8,82 @@ const realPodcasts = [
   },
 
   {
-    id: '91.2',
+    id: '90',
     img: '2.jpg',
     title: 'Falaaa Dev!!!',
     description:
       'Desenvolvimento Javascript em geral. Apresentado pelo pessoal da Rocketseat com uma das melhores tecnologias em programação, direto ao ponto e do jeito certo.',
   },
   {
-    id: '92.3',
+    id: '91.5',
     img: '3.jpg',
     title: 'Lambda3',
     description:
       'Desenvolvimento e diversidade. Apresentado pelo pessoal da lambda3. Onde apontam suas agilidades e como aplicam os problemas reais no dia a dia para desenvolver software. Que vão desde as origens históricas até o encerramendo do projeto de forma ágil com muito bom humor e versatilidade',
   },
   {
-    id: '93.9',
+    id: '92.2',
     img: '4.png',
     title: '#DevNaEstrada',
     description:
       'O podcast mais tradicional sobre desenvolvimento! Toda sexta-feira um novo episódio sobre tecnologias, entrevistas com profissionais na área, tudo com muito humor e alegria.',
   },
   {
-    id: '95.8',
+    id: '93.1',
     img: '5.jpg',
     title: 'Hipsters.tech',
     description:
-      'Desenvolvimento Javascript em geral. O Podcast de tecnologia “Hipsters ponto tech” é um projeto onde o pessoal da Caelum e da Alura conversa sobre startups, programação, ux, gadgets e as últimas tendências em tecnologia. O podcast foi criado em 2016 e já conta com mais de 164 episódios..Com isso, ganhando visibilidade no mundo Dev.',
+      'Desenvolvimento Javascript em geral. O Podcast de tecnologia “Hipsters ponto tech” é um projeto onde o pessoal da Caelum e da Alura conversa sobre startups, programação, ux e outros'
+      // gadgets e as últimas tendências em tecnologia. O podcast foi criado em 2016 e já conta com mais de 164 episódios..Com isso, ganhando visibilidade no mundo Dev.',
   },
 
   {
-    id: '98.3',
+    id: '94.1',
     img: '6.jpg',
     title: 'NerdCast',
     description:
-      'Cultura Nerd. Apresentado pelo Alexandre “Jovem Nerd” Ottoni e Deive “Azaghal”. O Nerdcast completou 10 anos em 2016 com 1 milhão de downloads por episódio e como primeiro lugar de audiência em podcasts no Brasil. Os assuntos são história, ciência, cinema, quadrinhos, literatura, tecnologia, games, RPG… Tudo que um nerd gosta! A conversa é informal, divertida, escrachada e tão doida quanto séria.',
+      'Cultura Nerd. Apresentado pelo Alexandre “Jovem Nerd” Ottoni e Deive “Azaghal”. O Nerdcast completou 10 anos em 2016 com 1 milhão de downloads por episódio'
+    //e como primeiro lugar de audiência em podcasts no Brasil. Os assuntos são história, ciência, cinema, quadrinhos, literatura, tecnologia, games, RPG… Tudo que um nerd gosta! A conversa é informal, divertida, escrachada e tão doida quanto séria.',
   },
 
   {
-    id: '100.4',
+    id: '95.5',
     img: '7.png',
     title: 'Área de tranferência.',
     description:
-      'Tecnologia em geral. Um podcast semanal sobre tecnologia, feito por 4 podcasters que já respiram o assunto diariamente. Bruno Casemiro, Guilherme Rambo, Gustavo Faria e Marcus Mendes comentam notícias sobre o mercado de tecnologia',
+      'Tecnologia em geral. Um podcast semanal sobre tecnologia, feito por 4 podcasters que já respiram o assunto diariamente: Bruno Casemiro, Guilherme Rambo, Gustavo Faria e Marcus Mendes'
+      //comentam notícias sobre o mercado de tecnologia',
   },
 
   {
-    id: '101.3',
+    id: '96.2',
     img: '8.jpg',
     title: 'Tecnocast',
     description:
-      'Tecnologia em geral. Apresentado por Tiago Mobilon, o tecnocast discute as últimas notícias do mundo da tecnologia trazendo avaliações e comentários pertinentes acerca dos lançamentos e novidades do meio. O podcast conta com mais de 120 episódios recheados de conteúdo tecnológico.',
+      'Tecnologia em geral. Apresentado por Tiago Mobilon, o tecnocast discute as últimas notícias do mundo da tecnologia trazendo avaliações e comentários pertinentes acerca dos lançamentos e novidades'
+      // do meio. O podcast conta com mais de 120 episódios recheados de conteúdo tecnológico.',
   },
   {
-    id: '102.5',
+    id: '97',
     img: '9.jpg',
     title: 'Loop matinal',
     description:
       'Tecnologia em geral. O podcast apresentado por Marcus Mendes posta diariamente novidades do mundo da tecnologia, fazendo um loop nas principais notícias e atualizações dos aplicativos.',
   },
   {
-    id: '104.1',
+    id: '98.1',
     img: '10.jpg',
     title: 'Podcast Canaltech',
     description:
-      'O Canaltech podcast é uma espécie de “jornal” diário com o resumo das principais notícias sobre tecnologia. Os assuntos tratados são: lançamentos do mercado, a movimentação das principais empresas do segmento, novidades das redes sociais, curiosidades, cultura geek, e muito mais.',
+      'O Canaltech podcast é uma espécie de “jornal” diário com o resumo das principais notícias sobre tecnologia. Os assuntos tratados são: lançamentos do mercado, a movimentação das principais empresas do segmento, novidades das redes sociais, curiosidades, cultura geek e muito mais.',
   },
   {
-    id: '107.1',
+    id: '99',
     img: '11.png',
     title: 'Bluesoft Podcast',
     description:
-      'A Bluesoft Labs é uma empresa do ramo de desenvolvimento de softwares que possui um podcast sobre tecnologia. Através deste canal, a Bluesoft Labs já conversou com diversos convidados sobre startups, internet das coisas e modelos de negócios disruptivos.',
+      'A Bluesoft Labs é uma empresa do ramo de desenvolvimento de softwares que possui um podcast sobre tecnologia. Através deste canal, a Bluesoft Labs já conversou com diversos convidados sobre startups'
+      // internet das coisas e modelos de negócios disruptivos.',
   },
 ];
 
