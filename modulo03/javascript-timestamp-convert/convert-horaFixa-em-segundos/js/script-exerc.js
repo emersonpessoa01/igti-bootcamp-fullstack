aec4237bfe227c1a8cdec91061da327ce8a2eaa5
emersonpessoa01 <eps011108@gmail.com>
2020-09-14 12:53:43 -0300

    yarn-integrity

--- modulo03/javascript-timestamp-convert/convert-horaFixa-em-segundos/js/script-exerc.js
+++ modulo03/javascript-timestamp-convert/convert-horaFixa-em-segundos/js/script-exerc.js
@@ -12,7 +12,7 @@ const leftPad = (value, count = 2, char = "0") => {
   let stringValue = value.toString();
   let newValue = stringValue;
 
-  if (stringValue.length < count) {
+  if (stringValue.length < count || stringValue.length % 10 ===0) {
     for (let i = 0; i < count - stringValue.length; i++) {
       newValue = char + stringValue;
     }
