85e4a7fad14606caf459b7c19a6729d8d6856c1a
emersonpessoa01 <eps011108@gmail.com>
2020-09-21 09:57:14 -0300

    insert router

--- modulo02/my-bank-api/src/index.js
+++ modulo02/my-bank-api/src/index.js
@@ -2,64 +2,11 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const port = 3000;
+const accountsRouter = require("./routes/accouny.js");
 
 app.use(express.json());
 
-app.post("/account", (req, res) => {
-  let account = req.body;
-  // console.log("post account");
-  fs.readFile("accounts.json", "utf8", (err, data) => {
-    // console.log(err);
-    if (!err) {
-      try {
-        let json = JSON.parse(data);
-        // console.log(json);
-        account = {
-          id: json.nextId++,
-          ...account,
-        };
-        json.accounts.push(account);
 
-        fs.writeFile("accounts.json", JSON.stringify(json), (err) => {
-          if (err) {
-            // console.log(err);
-            res.status(400).send({
-              error: err.message,
-            });
-          } else {
-            // res.end();
-            res.send("Tudo OK! DEV");
-          }
-        });
-      } catch (err) {
-        res.status(400).send({
-          error: err.message,
-        });
-      }
-    } else {
-      // console.log("erro na leitura");
-      // res.send("erro na leitura");
-      res.status(400).send({
-        error: err.message,
-      });
-    }
-  });
-});
-
-app.get("/account", (_, res) => {
-  fs.readFile("accounts.json", "utf8", (err, data) => {
-    if (!err) {
-      let json = JSON.parse(data)
-      delete json.nextId
-      res.send(json)
-      
-    } else {
-      res.status(400).send({
-        error: err.message,
-      });
-    }
-  });
-});
 
 app.listen(port, () => {
   try {
