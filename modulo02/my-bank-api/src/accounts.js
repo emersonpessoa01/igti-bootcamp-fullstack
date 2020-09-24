ae86afb7d5c1e959d03d2ed9efabc54bd2c8a9be
emersonpessoa01 <eps011108@gmail.com>
2020-09-24 14:31:19 -0300

    update transaction 02

--- modulo02/my-bank-api/src/accounts.js
+++ modulo02/my-bank-api/src/accounts.js
@@ -166,7 +166,7 @@ router.post("/transaction", (req, res) => {
         (account) => account.id === params.id
       );
 
-      if((params.value < 0) && ((json.accounts[index].balance - params.value)< 0)) {
+      if((params.value < 0) && ((json.accounts[index].balance + params.value)< 0)) {
         throw new Error("Não há saldo suficiente")
       }
       // res.send(index);
