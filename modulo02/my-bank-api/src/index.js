14a468a20f98b19bccf851541de8d9eef6909d93
emersonpessoa01 <eps011108@gmail.com>
2020-09-24 19:39:01 -0300

    upload log 01

--- modulo02/my-bank-api/src/index.js
+++ modulo02/my-bank-api/src/index.js
@@ -3,8 +3,29 @@ const fs = require("fs").promises;
 const app = express();
 const port = 3000;
 const accountsRouter = require("./accounts");
+const winston = require("winston");
 
 global.fileName = "accounts.json";
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ timestamp, label, level ,message }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.winston = winston.createlogger({
+  level: "silly",
+
+  //local para onde serão transportados
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File(),
+    {
+      filename: my - banck - app.log,
+    },
+  ],
+  //impressão dos logs
+  format: combine(winston.format),
+});
 
 app.use(express.json());
 app.use("/account", accountsRouter);
@@ -12,14 +33,14 @@ app.use("/account", accountsRouter);
 app.listen(port, async () => {
   try {
     await fs.readFile(global.fileName, "utf8");
-    console.log("Tudo OK! DEV");
+    logger.info("API started! DEV");
   } catch (err) {
     const initialJson = {
       nextId: 1,
       accounts: [],
     };
     fs.writeFile(global.fileName, JSON.stringify(initialJson)).catch((err) => {
-      console.log(err);
+      logger.error(err);
     });
   }
 });
