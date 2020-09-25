369c1acd81ff6f53ced31885e5f503201edde61d
emersonpessoa01 <eps011108@gmail.com>
2020-09-25 14:59:41 -0300

    upload components pictures

--- modulo03-React/functional components/react-desafio-03/frontend/src/components/Candidate.js
+++ modulo03-React/functional components/react-desafio-03/frontend/src/components/Candidate.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Picture from "./Picture";
 import Position from "./Position";
 
 export default function Candidate({ candidate, position }) {
-  const { name, votes } = candidate;
+  const { id, name, votes } = candidate;
+  const sourceImage = `${id}.jpg`
   return (
     <div>
       <Position>{position}</Position>
+      <Picture sourceImage={sourceImage} description={name}/>
       {name} - {votes}
     </div>
   );
