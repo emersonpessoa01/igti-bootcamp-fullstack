32a7b670ca33f1551c859bbb9d7c871635ef858f
emersonpessoa01 <eps011108@gmail.com>
2020-10-01 17:01:00 -0300

    convert users

--- modulo03-React/hooks/react-lifecycle/src/components/users/Users.js
+++ modulo03-React/hooks/react-lifecycle/src/components/users/Users.js
@@ -1,54 +1,31 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import User from "./User";
 
-export default class Users extends Component {
-  constructor() {
-    super();
+export default function Users({ users }) {
+  const [secondsVisible, setSecondsVisible] = useState(0);
 
-    this.state = {
-      secondsVisible: 0,
-    };
-    this.interval = null;
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsVisible(secondsVisible + 1)
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [secondsVisible])
 
-  componentDidMount() {
-    console.log("componentDidMont de Users.js");
-
-    this.interval = setInterval(() => {
-      const { secondsVisible } = this.state;
-      this.setState({
-        secondsVisible: secondsVisible + 1,
-      });
-    }, 1000);
-  }
-  componentDidUpdate() {
-    console.log("componentDidUpdate de Users.js");
-  }
-  componentWillUnmount() {
-    //console.log("componentWillUnmount de Users.js");
-    clearInterval(this.interval);
-  }
-
-  render() {
-    //console.log(this.props.users)
-    //console.log(users);
-    const { users } = this.props;
-    const { secondsVisible } = this.state;
-
-    return (
-      <div>
-        <p>Components Users visível por {secondsVisible} segundos</p>
-        <ul>
-          {users.map((user) => {
-            const { login } = user;
-            return (
-              <li key={login.uuid}>
-                <User user={user} />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>Components Users visível por {secondsVisible} segundos</p>
+      <ul>
+        {users.map((user) => {
+          const { login } = user;
+          return (
+            <li key={login.uuid}>
+              <User user={user} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
