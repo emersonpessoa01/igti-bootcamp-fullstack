fe7191553929d0456f379af42115d20529d7163a
emersonpessoa01 <eps011108@gmail.com>
2020-10-06 11:51:15 -0300

    insert timestamp in accounts.js

--- modulo02-Node.Js/my-bank-api/src/accounts.js
+++ modulo02-Node.Js/my-bank-api/src/accounts.js
@@ -5,6 +5,31 @@ const router = express.Router();
 const readFile = promises.readFile
 const writeFile = promises.writeFile
 
+//para converter o timestamp para horário da máquina local
+const leftPad = (value, count = 2, char = "0") => {
+  let stringValue = value.toString();
+  let newValue = stringValue;
+
+  if (stringValue.length < count || stringValue.length % 10 === 0) {
+    for (let i = 0; i < count - stringValue.length; i++) {
+      newValue = char + stringValue;
+    }
+  }
+  return newValue;
+};
+
+const now = new Date()
+const timer = `${leftPad(now.getDate())}/${leftPad(now.getMonth() + 1)}/${leftPad(now.getFullYear())}`;
+const hours = leftPad(now.getHours());
+const minutes = leftPad(now.getMinutes());
+const seconds = leftPad(now.getSeconds());
+
+const formatter = `${hours}:${minutes}:${seconds}`;
+const tt = formatter.split(":");
+const sec = tt[0] * 3600 + tt[1] * 60 + tt[2] * 1;
+const display = `${timer} ${formatter}`
+
+
 
 router.post("/", async (req, res) => {
   let account = req.body;
@@ -14,7 +39,7 @@ router.post("/", async (req, res) => {
 
     account = {
       id: json.nextId++,
-      ...account,
+      ...account, timestamp: display,
     };
     json.accounts.push(account);
 
