281b00ab1ab995eac8822ea6ead6be1400805d15
emersonpessoa01 <eps011108@gmail.com>
2020-10-22 18:51:53 -0300

    getSmallCityName

--- modulo02-Node.Js/cidades-estados/index.js
+++ modulo02-Node.Js/cidades-estados/index.js
@@ -1,6 +1,3 @@
-//método que imprima no console um array com a cidade de menor nome de cada estado
-// allStatesShorterCityName();
-
 // método que imprima no console a cidade de maior nome entre todos os estados
 // biggerNameCity()
 
@@ -14,7 +11,7 @@ const writeFile = promises.writeFile;
 const statesFile = "Estados.json";
 const citiesFile = "Cidades.json";
 
-// método que cria JSON pra cada estado com as cidades dentro
+//1 - método que cria JSON pra cada estado com as cidades dentro
 // createFiles()
 const createFile = async () => {
   let states = JSON.parse(await readFile(statesFile, "utf-8"));
@@ -31,19 +28,17 @@ const createFile = async () => {
 };
 createFile();
 
-// método que recebe como parâmetro o UF do estado e retorna quantidade de cidades
+//2 - método que recebe como parâmetro o UF do estado e retorna quantidade de cidades
 const getCitiesCount = async (uf) => {
   let cities = JSON.parse(await readFile(`./states/${uf}.json`, "utf8"));
   // console.log(cities);
-  console.log(
-    `UF: ${uf}, count: ${cities.length} cidades`
-  );
+  console.log(`2 - UF: ${uf}, count: ${cities.length} cidades`);
   return cities.length;
 };
 // getCitiesCount("PA");
-getCitiesCount(); //QUANTIDADE CIDADE DE "PA"
+getCitiesCount("PA"); //QUANTIDADE CIDADE DE "PA"
 
-// método que imprima no console um array com o UF dos cinco estados que mais possuem cidades
+//3 -  método que imprima no console um array com o UF dos cinco estados que mais possuem cidades
 // statesMoreCities();
 
 const getStateWithMoreCities = async (more) => {
@@ -69,28 +64,56 @@ const getStateWithMoreCities = async (more) => {
     //list.slice(0,5)//pega de 0 até 5a posição
     const result = [];
 
-    if (more) {
-      list.slice(0, 5).forEach(({ uf, count }) => {
-        return result.push(
-          `5 primeiro Estados com MENOR município - ${uf}: ${count}`
-        );
-      });
-    } else {
-      list.slice(-5).forEach(({ uf, count }) => {
-        return result.push(
-          `5 primeiro Estados com MAIOR município - ${uf}: ${count}`
-        );
-      });
-    }
+    list.slice(0, 5).forEach(({ uf, count }) => {
+      return result.push({ uf, count });
+    });
 
     // console.log(list.slice(0, 5));
+    // console.log("5 primeiro Estados com MENOR/MAIOR município -")
+    console.log("3 - MENORES MUNICíPIO");
     console.log(result);
   });
 };
-getStateWithMoreCities(true);
-getStateWithMoreCities(false);
+// getStateWithMoreCities(true);
+getStateWithMoreCities();
 
-// método que imprima no console um array com a cidade de maior nome de cada estado
+const getStateWithAnyLessCities = async (more) => {
+  let states = JSON.parse(await readFile(statesFile, "utf8"));
+  const list = [];
+
+  states.forEach(async (state) => {
+    let uf = state.Sigla;
+    let count = await getCitiesCount(uf);
+    list.push({
+      uf,
+      count,
+    });
+
+    //ordenando do maior para o menor
+    list.sort((a, b) => {
+      if (a.count < b.count) return -1;
+      else if (a.count > b.count) return 1;
+      else return 0;
+    });
+
+    //Selecionando os 5 primeiro Estados com maior município
+    //list.slice(0,5)//pega de 0 até 5a posição
+    const result = [];
+
+    list.slice(-5).forEach(({ uf, count }) => {
+      return result.push({ uf, count });
+    });
+
+    // console.log(list.slice(0, 5));
+    // console.log("5 primeiro Estados com MENOR/MAIOR município -")
+    console.log("3 - MAIORES MUNICíPIO");
+    console.log(result);
+  });
+};
+// getStateWithMoreCities(true);
+getStateWithAnyLessCities();
+
+//4 -  método que imprima no console um array com a cidade de maior nome de cada estado
 // allStatesBiggerCityName();
 const getBiggerName = async (uf) => {
   let cities = JSON.parse(await readFile(`./states/${uf}.json`, "utf8"));
@@ -121,8 +144,53 @@ const getBiggerCityName = async () => {
     let city = await getBiggerName(uf);
     result.push(`${city.Nome} - ${uf}`);
 
+    console.log("4 - cidades de maiores nome ");
     console.log(result);
   });
 };
 
 getBiggerCityName();
+
+//5 - método que imprima no console um array com a cidade de menor nome de cada estado
+// allStatesShorterCityName();
+
+const getSmallName = async (uf) => {
+  let cities = JSON.parse(await readFile(`./states/${uf}.json`, "utf8"));
+
+  let result;
+
+  cities.forEach((city) => {
+    if (!result) result = city;
+    else if (city.Nome.length < result.Nome.length) result = city;
+    else if (
+      city.Nome.length === result.Nome.length &&
+      city.Nome.length < result.Nome.length
+    )
+      result = city;
+    // console.log(cities);
+  });
+
+  return result;
+};
+getSmallName();
+
+const getSmallCityName = async (bigger) => {
+  let states = JSON.parse(await readFile("Estados.json"));
+
+  let result = [];
+  states.forEach(async (state) => {
+    let uf = state.Sigla;
+    let city;
+    if (bigger) {
+      city = await getBiggerName(uf);
+    } else {
+      city = await getSmallName(uf);
+    }
+    result.push(`${city.Nome} - ${uf}`);
+
+    console.log("5 - cidades de menores nome ");
+    console.log(result);
+  });
+};
+
+getSmallCityName();
