0e6819f853a01bd4778454e4cceeead3a7c087f9
emersonpessoa01 <eps011108@gmail.com>
2020-10-25 19:14:02 -0300

    upload01

--- modulo03-React/hooks/react-podcast/src/App.js
+++ modulo03-React/hooks/react-podcast/src/App.js
@@ -1,50 +1,7 @@
-import React, { Component } from "react";
+import React from 'react';
 
-export default class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      paragraph1: "",
-      paragraph2: "",
-      sum: 0,
-    };
-  }
-
-  handleParagraph1 = (evt) => {
-    this.setState({
-      paragraph1: evt.target.value,
-    });
-  };
-
-  handleParagraph2 = (evt) => {
-    this.setState({
-      paragraph2: evt.target.value,
-    });
-  };
-
-  componentDidUpdate() {
-    console.log("ComponentDidUpdate");
-  }
-
-  render() {
+export default function App() {
     return (
-      <div>
-        <h1>React componentDidUpdate</h1>
-
-        <input
-          type="text"
-          placeholder="Digite aqui"
-          value={paragraph1}
-          oncChange={this.handleParagraph1}
-        />
-        <input
-          type="text"
-          placeholder="Digite aqui"
-          value={paragraph2}
-          oncChange={this.handleParagraph2}
-        />
-      </div>
-    );
-  }
+      <div>Fala Dev!</div>
+    )
 }
