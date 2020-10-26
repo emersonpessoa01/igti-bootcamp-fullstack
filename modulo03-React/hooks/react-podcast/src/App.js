883229f47584c1aa917bb7e63efe03056e79ccf0
emersonpessoa01 <eps011108@gmail.com>
2020-10-25 22:00:55 -0300

    Update App.js

--- modulo03-React/hooks/react-podcast/src/App.js
+++ modulo03-React/hooks/react-podcast/src/App.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { Component } from "react";
 
-export default function App() {
+export default class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      paragraph1: "",
+      paragraph2: "",
+      sum: 0,
+    }
+  }
+
+  handleComponentDidUpdate1=(evt) => {
+    setState({
+	paragraph1: evt.target.value
+	})
+  }
+  handleComponentDidUpdate2=(evt) => {
+    setState({
+	paragraph2: evt.target.value
+	})
+  }
+
+  componentDidUpdate(_, previousState) {
+    console.log("componetDidUpdate")
+	document.title = this.state.paragraph1.length
+  }
+
+  render() {
+    const { paragraph1, paragraph2, value} =this.state
+
     return (
-      <div>Fala Dev! beleza</div>
-    )
+      <div>
+        <h1>React componentDidUpdate</h1>
+        <input
+          type="text"
+          placeholder="Digite aqui"
+          value={paragraph1}
+          onChange={this.handleComponentDidUpdate1}
+        />
+        <input
+          type="text"
+          placeholder="Digite aqui"
+          value={paragraph2}
+          onChange={this.handleComponentDidUpdate2}
+        />
+	<p>parágrafo 1: {paragraph1}</p>
+	<p>parágrafo 2: {paragraph2}</p>
+	<p>Quantidades de caracteres do parágrafo 2: {sum}</p>
+      </div>
+    );
+  }
 }
+
+
