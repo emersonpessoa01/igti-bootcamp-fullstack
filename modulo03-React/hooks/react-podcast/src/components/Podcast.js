26f078449db78884a790ebf1d7d7fd3dcb5e5f58
emersonpessoa01 <eps011108@gmail.com>
2020-10-27 15:07:24 -0300

    insert img, title and description

--- modulo03-React/hooks/react-podcast/src/components/Podcast.js
+++ modulo03-React/hooks/react-podcast/src/components/Podcast.js
@@ -2,9 +2,19 @@ import React, { Component } from "react";
 
 export default class Podcast extends Component {
   render() {
-    const { value } = this.props;
+    const { value } = this.props; //dispara a função selectedPodcast
+
+    if (!value) {
+      return <p>nenhum podcast encontado </p>;
+    }
+    const { img, title, description } = value;
     // return <div>{JSON.stringify(value) || "nenhum podcast"}</div>;
-    return <div>{JSON.stringify(value) || "nenhum podcast"}</div>;
+    return (
+      <div>
+        <img src={`./img/${img}`} />
+        <p>{title}</p>
+        <p>{description}</p>
+      </div>
+    );
   }
 }
-  
