ac5f27fe60ccf5c299c6b4024f3d46855f9adfe5
emersonpessoa01 <eps011108@gmail.com>
2020-10-30 10:06:17 -0300

    update 2003.json

--- modulo02-Node.Js/brasileirao/src/index.js
+++ modulo02-Node.Js/brasileirao/src/index.js
@@ -15,13 +15,33 @@ const init = async () => {
 
     //preenchendo a pontuacao de times no array de times
     //iterar rodada: pegar cada rodada e executar alguma coisa pra elas
-    data.forEach(rodada =>{
-      rodada.partidas.forEach(partida =>{
-        if(partida.placar_mandante> partida.placar_visitante){
-          times.findIndex(item => item.time ===partida.placar_mandante)
+    data.forEach((rodada) => {
+      rodada.partidas.forEach(
+        // const {placar_mandante, placar_visitante, mandante} = partida;
+
+        ({ placar_mandante, placar_visitante, mandante, visitante }) => {
+          const indexMandante = times.findIndex(
+            (item) => item.time === mandante
+          ); //encontrar posicao para pontuar o mandante
+          const indexVisitante = times.findIndex(
+            (item) => item.time === visitante
+          );//pontuar visitante
+          let timeMandante = times[indexMandante];//atualizar no array
+          let timeVisitante = times[indexVisitante];//atualizar no array
+
+          if (placar_mandante > placar_visitante) {
+            timeMandante.pontuacao += 3;
+            times[indexMandante] = timeMandante
+          } else if (placar_visitante > placar_mandante) {
+            timeVisitante.pontuacao = +3;
+            times[indexVisitante] = timeVisitante
+          } else {
+            timeMandante.pontuacao += 1;
+            timeVisitante.pontuacao += 1;
+          }
         }
-      })
-    })
+      );
+    });
     console.log(times);
   } catch (err) {
     console.log(err);
