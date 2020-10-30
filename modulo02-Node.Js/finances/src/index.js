7047cd29b0890f52f28258e96d96886caca1f815
emersonpessoa01 <eps011108@gmail.com>
2020-10-30 17:45:24 -0300

    inserindo writeFile

--- modulo02-Node.Js/finances/src/index.js
+++ modulo02-Node.Js/finances/src/index.js
@@ -1,10 +1,28 @@
-import express from "express";
+/**
+ * const express = require('express')
+ * const {writeFile} = require('fs').promises
+ */
 
-const port = 3000
+import express from "express";
+import { promises } from "fs";
+
+const { writeFile } = promises;
+const port = 3000;
 const app = express();
 app.use(express.json());
 
-app.listen(port,(req, res)=>{
-  console.log("Fala Dev")
+app.listen(port, async () => {
+try {
+    const initialJson = {
+      nextId: 1,
+      lancamentos: [],
+    };
+    //criando arquivo json a partir da constante initialJson
+    //passando a flag "wx". Executa só se existir
+    await writeFile("lancamentos.json", JSON.stringify(initialJson));
+  
+} catch (err) {
+  console.log(err)
+}
+  console.log("Fala Dev");
 });
-
