d88979ccb42d4d87df947a76239680241c9ae26f
emersonpessoa01 <eps011108@gmail.com>
2020-10-31 16:52:06 -0300

    inserindo funcoes genericas

--- modulo02-Node.Js/interativa_2/finances/src/lancamentos.js
+++ modulo02-Node.Js/interativa_2/finances/src/lancamentos.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { promises } from "fs";
+import calc from "../calculos.js"; //cal exportado como padrao
+//import {media, somatorio } "./calculos.js" --importacao sem padrao
 
 const { readFile, writeFile } = promises;
 const router = express.Router();
+
+router.get("/totalMes", (req, res) => {
+  res.send({valor: calc.somatorio([1,2,3,4])});
+});
+
 
 router.post("/receita", async (req, res) => {
   let lancamento = req.body;
@@ -10,17 +17,18 @@ router.post("/receita", async (req, res) => {
     let json = JSON.parse(await readFile("lancamentos.json"));
     // delete json.nextId;
 
-    lancamento = { 
-      id: json.nextId++, 
-      ...lancamento };
+    lancamento = {
+      id: json.nextId++,
+      ...lancamento,
+    };
     json.lancamentos.push(lancamento);
 
     await writeFile("lancamentos.json", JSON.stringify(json));
-    res.send(lancamento)
+    res.send(lancamento);
   } catch (err) {
     res.status(400).send({
-      error: err.message
-    })
+      error: err.message,
+    });
   }
 });
 
@@ -30,20 +38,21 @@ router.post("/despesa", async (req, res) => {
     let json = JSON.parse(await readFile("lancamentos.json"));
     // delete json.nextId;
 
-    lancamento = { 
-      id: json.nextId++, 
-      ...lancamento };
-    lancamento.valor = lancamento.valor * -1
+    lancamento = {
+      id: json.nextId++,
+      ...lancamento,
+    };
+    lancamento.valor = lancamento.valor * -1;
     json.lancamentos.push(lancamento);
 
     await writeFile("lancamentos.json", JSON.stringify(json));
     // res.end()
     // res.send("Inclusão realizado com sucesso")
-    res.send(lancamento)
+    res.send(lancamento);
   } catch (err) {
     res.status(400).send({
-      error: err.message
-    })
+      error: err.message,
+    });
   }
 });
 
