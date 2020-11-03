f6833b3e41c196cf2a11f4dcd3dd87a240756df1
emersonpessoa01 <eps011108@gmail.com>
2020-11-03 16:59:30 -0300

    defining flexWrap

--- modulo03-React/hooks/react-megasena/src/components/Numbers.js
+++ modulo03-React/hooks/react-megasena/src/components/Numbers.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import Number from '../Number'
+import React from "react";
+import Number from "../Number";
 
+export default function Numbers({ numbers }) {
+  const { container} = styles;
 
-export default function Numbers({numbers}) {
   return (
-    <div>
-      {numbers.map(number => {
-        return (
-          < Number key={number.id}/>
-        )
+    <div style={container}>
+      {numbers.map((number) => {
+        return <Number key={number.id} />;
       })}
     </div>
-  )
+  );
 }
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+
+    //pra quebrar no flex
+    flexWrap: "wrap",
+  },
+};
