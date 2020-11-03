77a9c1b50c9bb6f66f6c84dfdfae5c55e8ce490f
emersonpessoa01 <eps011108@gmail.com>
2020-11-03 17:24:38 -0300

    inserting css in Number.js

--- modulo03-React/hooks/react-megasena/src/Number.js
+++ modulo03-React/hooks/react-megasena/src/Number.js
@@ -10,3 +10,46 @@ export default function Number({number}) {
     </div>
   )
 }
+
+const styles ={
+  container: {
+    border: "1px solid lightgray",
+    borderRadius:"4px",
+    padding: "4px",
+    margin: "5px",
+
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    width: "80px",
+    flexWrap: "wrap",
+
+  },
+
+  number: {
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    marginRight: "10px",
+
+  },
+
+  badgeContainer: {
+    minWidth: "30px",
+    minHeight: "30px",
+    border: "1px solid transparent",
+    borderRadius: "50%",
+    backgroundColor: "#DD0031",
+
+    padding: '4px',
+    display:"flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  badgeStyle:{
+    fontSize:"0.8rem",
+    fontWeight:"bold",
+    color: "white",
+  }
+}
