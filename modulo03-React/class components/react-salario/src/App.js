fbcd3ee846d23f86f8a7fa25357e3e1bd1f79d5b
emersonpessoa01 <eps011108@gmail.com>
2020-11-05 16:14:37 -0300

    upload app salario

---
+++ modulo03-React/class components/react-salario/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      fullSalary: 1000,
+      
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <h1>React Salário</h1>
+    )
+  }
+}
---
+++ modulo03-React/class components/react-salario/src/components/InputFullSalary.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class InputFullSalary extends Component {
+  render() {
+    return (
+      <div className="input-field col 12">
+        
+        
+      </div>
+    )
+  }
+}
