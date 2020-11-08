db1b0cf8f1637d89a0017a9ea896d6db7dcce625
emersonpessoa01 <eps011108@gmail.com>
2020-11-08 20:40:54 -0300

    update swagger

--- modulo02-Node.Js/interativa_2/finances/src/docs.js
+++ modulo02-Node.Js/interativa_2/finances/src/docs.js
@@ -13,7 +13,7 @@ export const swaggerDocument = {
     }
   ],
   "paths": {
-    "/lancamentos": {
+    "/lancamentos/totalMes/mes": {
       "get": {
         "tags": [
           "lancamentos"
@@ -38,152 +38,25 @@ export const swaggerDocument = {
           }
         }
       },
-      "post": {
-        "tags": [
-          "lancamentos"
-        ],
-        "summary": "Create new lancamentos",
-        "description": "Create new lancamentos with the received parameters",
-        "consumes": [
-          "application/json"
-        ],
-        "parameters": [
-          {
-            "in": "body",
-            "name": "body",
-            "description": "Lancamentos object",
-            "required": true,
-            "schema": {
-              "$ref": "#/definitions/Lancamentos"
-            }
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "Successsfull operation"
-          },
-          "400": {
-            "description": "Error occurred"
-          }
-        }
-      },
-      "put": {
-        "tags": [
-          "lancamentos"
-        ],
-        "summary": "Updated lancamentos",
-        "description": "Updated lancamentos exists.",
-        "produces": [
-          "application/json"
-        ],
-        "parameters": [
-          {
-            "in": "body",
-            "name": "body",
-            "description": "lancamentos object",
-            "required": true,
-            "schema": {
-              "$ref": "#/definitions/lancamentosUser"
-            }
-          }
-        ],
-        "responses": {
-          "400": {
-            "description": "Invalid user supplied"
-          }
-        }
-      }
+      
+      
     },
-    "/lancamentos/{id}": {
-      "get": {
-        "tags": [
-          "lancamentos"
-        ],
-        "summary": "Get existing lancamentos",
-        "description": "Get existing lancamentos description",
-        "produces": [
-          "application/json"
-        ],
-        "parameters": [
-          {
-            "name": "id",
-            "in": "path",
-            "description": "ID of lancamentos",
-            "required": true,
-            "type": "integer",
-            "format": "int64"
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "Successsfull operation",
-            "schema": {
-              "$ref": "#/definitions/Lancamentos"
-            }
-          },
-          "400": {
-            "description": "Error occurred"
-          }
-        }
-      },
-      "delete": {
-        "tags": [
-          "lancamentos"
-        ],
-        "summary": "Delete existing lancamentos",
-        "description": "Delete existing lancamentos description",
-        "parameters": [
-          {
-            "name": "id",
-            "in": "path",
-            "description": "ID of lancamentos",
-            "required": true,
-            "type": "integer",
-            "format": "int64"
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "Successsfull operation"
-          },
-          "400": {
-            "description": "Error occurred"
-          }
-        }
-      }
-    }
+    
   },
   "definitions": {
     "Lancamentos": {
       "type": "object",
       "properties": {
-        "name": {
-          "type": "string",
-          "example": "Emerson Pessoa"
+        "function": {
+          "type": "number",
+          "example": "Somatorio"
         },
-        "balance": {
+        "Valor": {
           "type": "integer",
-          "example": 100
+          "example":[1,2,3]
         }
       }
     },
-    "LancamentosUser": {
-      "type": "object",
-      "properties": {
-        "id": {
-          "type": "integer",
-          "example": 1
-        },
-        "name": {
-          "type": "string",
-          "example": "Emerson Pessoa"
-        },
-        "balance": {
-          "type": "integer",
-          "example": 100
-        }
-      }
-    }
   }
 }
 
