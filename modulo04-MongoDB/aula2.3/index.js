f0106fb24a173dc5330153be406dd258a80ecc60
emersonpessoa01 <eps011108@gmail.com>
2020-11-10 16:47:05 -0300

    creation model

--- modulo04-MongoDB/aula2.3/index.js
+++ modulo04-MongoDB/aula2.3/index.js
@@ -1,6 +1,41 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost/api", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// mongoose.connect("mongodb+srv://emersonpessoa:salmo119@cluster0.cginj.mongodb.net/api?retryWrites=true&w=majority", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(
+//   console.log("Conectado ao MongoDB Atlas")
+// ).catch((err) => {
+//   console.log(`Erro ao conectar no Mongo DB Atlas ${err}`)
+// })
+
+// conectar ao MongoDB pelo mongoose
+await mongoose.connect(
+  "mongodb+srv://emersonpessoa:salmo119@cluster0.cginj.mongodb.net/api?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+//criacao do modelo
+const studentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  balance:{
+    type: Number,
+    required: true,
+  },
+  conta:{
+    type: Number,
+    required: true,
+  },
+  agencia:{
+    type: Number,
+    required: true,
+  },
 });
+
+mongoose.model("Student",studentSchema)
