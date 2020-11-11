6d56389c8bcfc23da8511f3d9c331ff99b0bdb32
emersonpessoa01 <eps011108@gmail.com>
2020-11-11 17:12:51 -0300

    route post created in studentRouter

--- modulo04-MongoDB/aula2.4/routes/studentRouter.js
+++ modulo04-MongoDB/aula2.4/routes/studentRouter.js
@@ -1,20 +1,32 @@
 //interface para o controller
 import express from "express";
-import { studentModel} from "./models/student.js"
+import { studentModel } from "./models/student.js";
 
 const app = express();
 
 //criando o create do CRUD
 app.get("/", async (req, res) => {
   // res.send({ result: "Validado" });
-
   try {
     const student = await studentModel.find({});
 
     res.send(student);
   } catch (err) {
-    res.send(400).send({ result: err.message });
+    res.status(400).send({ message: err.message });
   }
 });
+
+app.post('/', async (req, res)=>{
+  try {
+    const student = new studentModel(req.body);
+    await student.save();//para persistir na collection
+    
+    res.send(student);
+
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+    
+  }
+})
 
 export { app as studentRouter };
