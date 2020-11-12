53f912ab5b5992d478a405f7bcc1517f56513b6c
emersonpessoa01 <eps011108@gmail.com>
2020-11-12 17:50:01 -0300

    encapsulando variaveis de ambiente com dotenv

--- modulo04-MongoDB/aula2.4/app.js
+++ modulo04-MongoDB/aula2.4/app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import { studentRouter } from "./routes/studentRouter.js";
+
+require("dotenv").config();
+// import dotenv from "dotenv";
+
+
+//criando variaveis de ambiente
+// process.env.USER_DB = "emersonpessoa"
+
+
+
 //Conexao com o MongoDB
 (async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://emersonpessoa:salmo119@cluster0.cginj.mongodb.net/grades?retryWrites=true&w=majority",
+      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0.cginj.mongodb.net/grades?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -20,8 +30,7 @@ import { studentRouter } from "./routes/studentRouter.js";
 const app = express();
 app.use(express.json());
 app.use("/student", studentRouter);
-const port = 3016;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Fala Dev -- API STARTED");
 });
