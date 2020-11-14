8d5b237b83abe0e1c56562f4d03c6d424ccbfed4
emersonpessoa01 <eps011108@gmail.com>
2020-11-14 14:58:31 -0300

    delete package-lock.json

--- modulo04-MongoDB/aula2.2.4/app.js
+++ modulo04-MongoDB/aula2.2.4/app.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import mongoose from "mongoose";
 import { studentRouter } from "./routes/studentRouter.js";
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 //criando variaveis de ambiente
 // process.env.USER_DB = "emersonpessoa"
@@ -11,7 +11,7 @@ dotenv.config();
 (async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://emersonpessoa:salmo119@cluster0.cginj.mongodb.net/grades?retryWrites=true&w=majority",
+      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0.cginj.mongodb.net/grades?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
